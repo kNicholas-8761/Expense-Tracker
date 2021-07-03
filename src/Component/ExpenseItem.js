87554771662fdf9cb53ddf47f7b656d2c28c8a1e
user <kamauri84@gmail.com>
2021-07-03 17:25:09 +1000

    add styling to expense item component

--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -1,12 +1,18 @@
+import './ExpenseItem.css'
+
 const ExpenseItems = () => {
-    return (
+    const ExpenseDate = new Date('11/10/1984');
+    const Expense = 'Car Service';
+    const ExpenseAmount = 200;
+    
+  return (
+    <div className = 'expense-item'>
+      <div>{ExpenseDate.toISOString()}</div>
       <div>
-        <div>11/10/84</div>
-        <div>
-          <h1>Car Service</h1>
-          <div>$200</div>
-        </div>
+        <h2>{Expense}</h2>
+        <div className ="expense-item__price">${ExpenseAmount}</div>
       </div>
-    );
-  };
-  export default ExpenseItems;
+    </div>
+  );
+};
+export default ExpenseItems;
