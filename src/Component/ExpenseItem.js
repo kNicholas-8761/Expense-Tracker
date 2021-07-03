b3b5a96e049ba630feadbe4f90fb5625c2c2e7d3
user <kamauri84@gmail.com>
2021-07-03 17:20:14 +1000

    add expense item component

---
+++ src/Component/ExpenseItem.js
@@ -0,0 +1,12 @@
+const ExpenseItems = () => {
+    return (
+      <div>
+        <div>11/10/84</div>
+        <div>
+          <h1>Car Service</h1>
+          <div>$200</div>
+        </div>
+      </div>
+    );
+  };
+  export default ExpenseItems;
